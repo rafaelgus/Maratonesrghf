ce84c15f1042f03073716bdc993068619dd881f0
rafaelgus <rafaelgustavo1712@gmail.com>
2018-03-19 17:05:13 -0400

    Mas cambios
    aplicando cambios a la seccion mujeres
    creando la seccion el mas veloz
    aplicando responsive a las dos secciones

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import navigationOffCanvas from './components/navigation_off_canvas'
 import transparentHeader from './components/transparent_header'
+import headerVideo from './components/header_video'
 import youtubeVideo from './components/youtube_video'
 
 navigationOffCanvas()
