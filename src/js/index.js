bb9b0428acb880aee4596d03b0e8bcf6a9b67eed
rafaelgus <rafaelgustavo1712@gmail.com>
2018-03-18 21:30:51 -0400

    Culminando el home y iniciando seccion acerca
    Ya con el home culminado con todos sus estilos  e iniciando la seccion acerca

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import navigationOffCanvas from './components/navigation_off_canvas'
+import transparentHeader from './components/transparent_header'
 import youtubeVideo from './components/youtube_video'
 
 navigationOffCanvas()
+transparentHeader()
 youtubeVideo()
